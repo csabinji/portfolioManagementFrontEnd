79957a0416cfbf62876852a11886f8978ed7e742
sabin <csabinji@gmail.com>
2022-01-06 16:07:22 +0545

    navigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 import Body from './components/body';
+import Navigation from './components/navigation/navigation';
 
 function App() {
   return (
     <div className="App">
+      <Navigation /> <br />
       <Body />
     </div>
   );
