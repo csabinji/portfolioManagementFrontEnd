7a6fb2211878929b3601da8df6bc8d52f75b1014
sabin <csabinji@gmail.com>
2022-01-06 18:26:58 +0545

    buy/sell stock

--- src/components/order/order.jsx
+++ src/components/order/order.jsx
@@ -5,7 +5,11 @@ import './order.css';
 export default function Order() {
     const [stocks, setStocks] = useState([]);
     const [stockMatch, setStockMatch] = useState([]);
-    const [text, setText] = useState('')
+    const [text, setText] = useState('');
+    const [status, setStatus] = useState('buy');
+    const [total, setTotal] = useState('');
+    const [price, setPrice] = useState('');
+    const [date, setDate] = useState('');
 
     useEffect(() => {
         axios.get("http://localhost:90/getStocks")
@@ -33,17 +37,62 @@ export default function Order() {
         setStockMatch(matches)
     }
 
+    const order = (e) => {
+        e.preventDefault();
+
+        const data = { stockName: text, price, total, transactionDate: date }
+        console.log(data);
+
+        axios.post("http://localhost:90/buySellStock/" + status, data)
+            .then(res => {
+            }).catch(err => {
+                console.log(err)
+            });
+    }
+
     return (
-        <table>
-            <tr> <input type="search" placeholder='Enter any script'
-                onChange={(e) => searchStocks(e.target.value)}
-                value={text} /></tr>
-            {stockMatch && stockMatch.map((stock, index) => (
-                <div key={index} className='col-md-12 justify-content-md-center suggestion'
-                    onClick={() => onSuggestionHandler(stock.stockName)}>
-                    {stock.stockName}
-                </div>
-            ))}
+        <table className='container'>
+            <tr>
+                <td><input type="text" placeholder='Enter any script'
+                    onChange={(e) => searchStocks(e.target.value)}
+                    value={text} />
+                </td>
+                <td>
+                    <input type="text" placeholder='Quantity'
+                        onChange={(e) => setTotal(e.target.value)}
+                    />
+                </td>
+                <td>
+                    <select onChange={(e) => {
+                        const selectedItem = e.target.value;
+                        setStatus(selectedItem)
+                    }}>
+                        <option value="buy">Buy</option>
+                        <option value="sell" onClick={(e) => setStatus('sell')}>Sell</option>
+                    </select>
+                </td>
+                <td>
+                    <input type="text" placeholder='Price'
+                        onChange={(e) => setPrice(e.target.value)}
+                    />
+                </td>
+                <td>
+                    <input type="text" placeholder='Transaction Date'
+                        onChange={(e) => setDate(e.target.value)}
+                    />
+                </td>
+                <td>
+                    <button onClick={order}>Order</button>
+                </td>
+            </tr>
+            {
+                stockMatch && stockMatch.map((stock, index) => (
+                    <div key={index} className='col-md-12 justify-content-md-center suggestion'
+                        onClick={() => onSuggestionHandler(stock.stockName)}>
+                        {stock.stockName}
+                    </div>
+                ))
+            }
         </table>
     )
 }
